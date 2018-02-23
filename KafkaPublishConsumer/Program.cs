b6712a2013e118e57e18fbe1e73e90bcf8cc52f6
akshey.goyal <akshey.goyal@EVS01CPB590>
2018-02-23 17:10:50 +0530

    Add project files.

---
+++ KafkaPublishConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;   
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace KafkaPublishConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new KafkaOptions(new Uri("http://evs01cpb592:9092"));
+            var router = new BrokerRouter(options);
+            var client = new KafkaNet.Producer(router);
+
+            client.SendMessageAsync("testtopic", new[]{ new Message("Hi Hello! Welcome to Kafka!") }).Wait();
+            Console.ReadLine();
+        }
+    }
+}
